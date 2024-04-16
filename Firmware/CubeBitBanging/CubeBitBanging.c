88d5065fc97fb991faafade92e315099a87e314b
HalfHeuslerAlloy <49373316+HalfHeuslerAlloy@users.noreply.github.com>
2024-04-16 17:22:51 +0100

    Update CubeBitBanging.c

--- Firmware/CubeBitBanging/CubeBitBanging.c
+++ Firmware/CubeBitBanging/CubeBitBanging.c
@@ -17,12 +17,90 @@
 
 #define dataPin 9
 
+void OutputFrame(bool frame[2][8][8][32][3], int frameSelect){
+
+    bool led = false;
+
+    const int ColSeq[3] = {1,3,0};
+    int c;
+
+    for(int k = 0; k < 32; k++){
+
+        //Turn off output while setting layer selector pins
+        gpio_put(outputPin,1);
+        busy_wait_at_least_cycles(10);
+
+        //Configure vertical column pins selection
+        if( (k & 0b00000001) != 0 ) {gpio_put(layerSelA,1);}
+                                else{gpio_put(layerSelA,0);}
+
+        if( (k & 0b00000010) != 0 ) {gpio_put(layerSelB,1);}
+                                else{gpio_put(layerSelB,0);}
+
+        if( (k & 0b00000100) != 0 ) {gpio_put(layerSelC,1);}
+                                else{gpio_put(layerSelC,0);}
+
+        if( (k & 0b00001000) != 0 ) {gpio_put(layerSelD,1);}
+                                else{gpio_put(layerSelD,0);}
+
+        // slightly different because disabling and enabling multiplexer IO chips
+        if( (k & 0b00010000) != 0 ){
+            gpio_put(layerSelE,0);
+            gpio_put(layerSelF,1);
+        }
+        else{
+            gpio_put(layerSelE,1);
+            gpio_put(layerSelF,0);
+        }
+
+        // Enable output again
+        gpio_put(outputPin,0);
+
+
+        // Load new layer data into the shift registers
+        for(int cn = 0; cn < 3; cn++){
+        for(int j = 0; j < 8; j++){
+            for(int i = 0; i < 8; i++){
+
+                c = ColSeq[cn];
+                led = frame[frameSelect][i][j][k][c];
+
+                led = !led; // Led on with low signal
+
+                gpio_put(dataPin,led);
+                busy_wait_at_least_cycles(10); // very short 10 cycle delay 10*8ns per cycle, ~80ns
+
+                //Pulse clk pin for at least 20ns
+                gpio_put(clkPin,1);
+                busy_wait_at_least_cycles(10);
+                gpio_put(clkPin,0);
+                busy_wait_at_least_cycles(10);
+
+            }
+        }
+        }
+
+        //Pulse load pin to move inputted data to display out
+        gpio_put(loadPin,1);
+        busy_wait_at_least_cycles(10);
+        gpio_put(loadPin,0);
+    }
+
+    gpio_put(25,1);
+    sleep_ms(100);
+    gpio_put(25,0);
+
+}
+
 int main() {
 
     // Define the two frames, each 8 by 8 by 32 pixels and 3 colors deep
-    bool frame[2][8][8][32][3] = 0;
+    bool frame[2][8][8][32][3] = {false};
 
     int frameSelect = 0;
+
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
 
     gpio_init(clkPin);
     gpio_set_dir(clkPin, GPIO_OUT);
@@ -58,8 +136,8 @@ int main() {
     gpio_put(layerSelB,0);
     gpio_put(layerSelC,0);
     gpio_put(layerSelD,0);
-    gpio_put(layerSelE,0);
-    gpio_put(layerSelF,0);
+    gpio_put(layerSelE,1); // Active low
+    gpio_put(layerSelF,0); // Active low
 
     int i = 0;
     int j = 0;
@@ -75,11 +153,13 @@ int main() {
             j = 0;
             k++;
         }
-        if(k==8){
+        if(k==32){
             k = 0;
         }
 
-        frame[0][i][j][k][0,1,3] = 1;
+        frame[0][i][j][k][0] = 1;
+        frame[0][i][j][k][1] = 1;
+        frame[0][i][j][k][2] = 1;
 
         OutputFrame(frame,frameSelect);
 
@@ -87,73 +167,4 @@ int main() {
      }
 
     return 0;
-}
-
-void OutputFrame(bool frame[2][8][8][32][3], int frameSelect){
-
-    bool led = false;
-
-    const int ColSeq[3] = {1,3,0};
-    int c;
-
-    for(int k = 0; k < 32; k++){
-
-        //Turn off output while setting layer selector pins
-        gpio_put(outputPin,1);
-        busy_wait_at_least_cycles(10);
-
-        //Configure vertical column pins selection
-        if( (k & 0b00000001) != 0 ) {gpio_put(layerSelA,1);}
-                                else{gpio_put(layerSelA,0);}
-
-        if( (k & 0b00000010) != 0 ) {gpio_put(layerSelB,1);}
-                                else{gpio_put(layerSelB,0);}
-
-        if( (k & 0b00000100) != 0 ) {gpio_put(layerSelC,1);}
-                                else{gpio_put(layerSelC,0);}
-
-        if( (k & 0b00001000) != 0 ) {gpio_put(layerSelD,1);}
-                                else{gpio_put(layerSelD,0);}
-
-        // slightly different because disabling and enabling multiplexer IO chips
-        if( (k & 0b00010000) != 0 ){
-            gpio_put(layerSelE,1);
-            gpio_put(layerSelF,0);
-        }
-        else{
-            gpio_put(layerSelE,0);
-            gpio_put(layerSelF,1);
-        }
-
-        // Enable output again
-        gpio_put(outputPin,0);
-
-
-        // Load new layer data into the shift registers
-        for(int cn = 0; cn < 3; cn++){
-        for(int j = 0; j < 8; j++){
-            for(int i = 0; i < 8; i++){
-
-                c = ColSeq[cn];
-                led = frame[frameSelect][i][j][k][c];
-
-                gpio_put(dataPin,led);
-                busy_wait_at_least_cycles(10); // very short 10 cycle delay 10*8ns per cycle, ~80ns
-
-                //Pulse clk pin for at least 20ns
-                gpio_put(clkPin,1);
-                busy_wait_at_least_cycles(10);
-                gpio_put(clkPin,0);
-                busy_wait_at_least_cycles(10);
-
-            }
-        }
-        }
-
-        //Pulse load pin to move inputted data to display out
-        gpio_put(loadPin,1);
-        busy_wait_at_least_cycles(10);
-        gpio_put(loadPin,0);
-    }
-
 }
